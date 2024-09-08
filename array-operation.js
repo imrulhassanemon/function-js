195c831c8d9562ae4f5b6d0ab19393f948de0899
imrulhassanemon <imrulhassan989@gmail.com>
2024-09-08 22:17:58 +0600

    added array operation

---
+++ array-operation.js
@@ -0,0 +1,30 @@
+/*
+ *Objective: write a function to give me the sum of all numbers in an array 
+*/
+// let sum = 0
+// function sumArray(array){
+//     for(const number of array){
+//         // console.log(number);
+//         sum = sum + number
+//     }
+//     return sum
+// }
+// const nums = [23, 25, 29, 30];
+// const total = sumArray(nums);
+// console.log("the sum of numbers is ", total);
+
+
+
+
+let sum = 0;
+
+function sumArray(array){
+    for(number of array){
+        console.log(number)
+        sum = sum + number
+    }
+    return sum
+}
+const arr = [2, 3, 4, 5]
+const total = sumArray(arr)
+console.log("sum of array", total)
--- various_params.js
+++ various_params.js
@@ -13,3 +13,29 @@ function evenSizeString(str){
 
 evenSizeString("Dhaka")
 evenSizeString("faka")
+
+
+
+function doubleOrTriple(number, double){
+    if(double){
+        return number * 2
+    }
+    else{
+        return number * 3 
+    }
+}
+
+const bul = doubleOrTriple(5, true)
+console.log("it will return duble ", bul)
+const bul2 = doubleOrTriple(10, false)
+console.log("It returns triple", bul2)
+
+
+console.log("--------------------------------------------------")
+
+function numberOfElements (numbers){
+    const len = numbers.length;
+    return len
+}
+
+console.log(numberOfElements([1, 2, 3, 4, 5, 7, 3, 2, 4, 2]))
